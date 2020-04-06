2c6a51affa4937958dac0d00d05a22a124735f49
mvboettcher <mvboettcher@gmail.com>
2020-04-05 23:35:05 -0400

    adds UpdateUserDialog, updates README.md

--- src/components/DirectoryTable/index.jsx
+++ src/components/DirectoryTable/index.jsx
@@ -7,71 +7,90 @@ import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
-import IconButton from '@material-ui/core/IconButton'
-
 import RemoveCircle from '@material-ui/icons/RemoveCircle'
 import Edit from '@material-ui/icons/Edit'
+
+import UpdateUserDialog from '../UpdateUserDialog'
 
 import { withStyles } from '@material-ui/core/styles'
 import styles from './styles'
 
-const EditDeleteButtons = ({ classes, deleteUser, id }) => (
-  <div className={classes.editDeleteContainer}>
-    <Edit fontSize='small' className={classes.editDeleteButton} />
-    <RemoveCircle
-      fontSize='small'
-      className={classes.editDeleteButton}
-      onClick={() => deleteUser(id)}
-    />
-  </div>
-)
+const EditDeleteButtons = ({ classes, deleteUser, user }) => {
+  const [dialogOpen, setDialogOpen] = useState(false)
 
-const DirectoryTable = ({ classes, users, deleteUser }) => {
+  const openDialog = () => setDialogOpen(true)
+  const closeDialog = () => setDialogOpen(false)
+
+  return (
+    <div className={classes.editDeleteContainer}>
+      <Edit
+        fontSize='small'
+        className={classes.editDeleteButton}
+        onClick={openDialog}
+      />
+      <RemoveCircle
+        fontSize='small'
+        className={classes.editDeleteButton}
+        onClick={() => deleteUser(user._id)}
+      />
+      <UpdateUserDialog
+        open={dialogOpen}
+        closeDialog={closeDialog}
+        user={user}
+      />
+    </div>
+  )
+}
+
+const DirectoryTable = ({ classes, users, deleteUser, updateUser }) => {
   const [hover, setHover] = useState(null)
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label='simple table'>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align='left'>Type</TableCell>
-            <TableCell align='center'>Sex</TableCell>
-            <TableCell align='center'>Email</TableCell>
-            <TableCell align='right'>Birthdate</TableCell>
-            <TableCell />
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((user, idx) => (
-            <TableRow
-              key={idx}
-              onMouseEnter={() => setHover(idx)}
-              onMouseLeave={() => setHover(null)}
-            >
-              <TableCell component='th' scope='row'>
-                {user.name}
-              </TableCell>
-              <TableCell align='left'>{user.type}</TableCell>
-              <TableCell align='center'>{user.sex}</TableCell>
-              <TableCell align='center'>{user.email}</TableCell>
-              <TableCell align='right'>{user.birthdate}</TableCell>
-              <TableCell align='right'>
-                {hover === idx ? (
-                  <EditDeleteButtons
-                    id={user._id}
-                    classes={classes}
-                    deleteUser={deleteUser}
-                  />
-                ) : (
-                  <div style={{ width: 40 }} />
-                )}
-              </TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align='left'>Type</TableCell>
+              <TableCell align='center'>Sex</TableCell>
+              <TableCell align='center'>Email</TableCell>
+              <TableCell align='right'>Birthdate</TableCell>
+              <TableCell />
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {users.map((user, idx) => (
+              <TableRow
+                key={idx}
+                onMouseEnter={() => setHover(idx)}
+                onMouseLeave={() => setHover(null)}
+              >
+                <TableCell component='th' scope='row'>
+                  {user.name}
+                </TableCell>
+                <TableCell align='left'>{user.type}</TableCell>
+                <TableCell align='center'>{user.sex}</TableCell>
+                <TableCell align='center'>{user.email}</TableCell>
+                <TableCell align='right'>{user.birthdate}</TableCell>
+                <TableCell align='right'>
+                  {hover === idx ? (
+                    <EditDeleteButtons
+                      classes={classes}
+                      user={user}
+                      deleteUser={deleteUser}
+                      updateUser={updateUser}
+                    />
+                  ) : (
+                    <div style={{ width: 40 }} />
+                  )}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   )
 }
 
---
+++ src/components/UpdateUserDialog/styles.js
@@ -0,0 +1,10 @@
+const styles = (theme) => ({
+  form: {
+    width: '100%'
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+})
+
+export default styles
